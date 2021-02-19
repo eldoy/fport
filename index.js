6fd43f94294f2c3f8fd4071b86b509c432515752
Vidar Eldøy <vidar@eldoy.com>
2021-02-19 18:59:10 +0100

    Add taken function

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const net = require('net')
+const Socket = net.Socket
 
-module.exports = function() {
+const fport = {}
+
+fport.port = function() {
   return new Promise((resolve, reject) => {
-    const srv = net.createServer()
-    srv.on('error', reject)
-    srv.listen(0, () => {
-      const { port } = srv.address()
-      srv.close(() => {
+    const server = net.createServer()
+    server.on('error', reject)
+    server.listen(0, () => {
+      const { port } = server.address()
+      server.close(() => {
         resolve(port)
       })
     })
   })
 }
+
+fport.taken = function(port, host = '127.0.0.1') {
+  return new Promise(resolve => {
+    var socket = new Socket()
+    socket.setTimeout(400)
+
+    socket.on('connect', function () {
+      socket.destroy()
+      resolve(true)
+    })
+
+    socket.on('timeout', function () {
+      socket.destroy()
+      resolve(false)
+    })
+
+    socket.on('error', function (e) {
+      resolve(false)
+    })
+
+    socket.connect(port, host)
+  })
+}
+
+module.exports = fport
--- test/fport.test.js
+++ test/fport.test.js
@@ -1,6 +1,11 @@
 const fport = require('../index.js')
 
 it('should find an open port', async () => {
-  const port = await fport()
+  const port = await fport.port()
   expect(port).toBeGreaterThan(1024)
+})
+
+it('should check if port is taken', async () => {
+  let result = await fport.taken(27674)
+  expect(result).toEqual(false)
 })
